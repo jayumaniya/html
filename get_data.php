6477c0308273647ed9309703501d3b50806342e2
lathiyaharmik <66663005+Lathiya50@users.noreply.github.com>
2021-10-03 12:03:54 +0530

    Update get_data.php
    just needed to remove else portion

--- get_data.php
+++ get_data.php
@@ -5,9 +5,7 @@ $type=$_POST['type'];
 
 if($type=='city'){
 	$sql="select id,name from city where state_id='$id'";
-}else{
-	$sql="select id,name from state where country_id='$id'";
-}
+}else
 $stmt=$con->prepare($sql);
 $stmt->execute();
 $arr=$stmt->fetchAll(PDO::FETCH_ASSOC);
